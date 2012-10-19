5ae31433a955752e82d021b03cdcb5a35a3877fd
Darío Blanco <dario.blanco@edelight.de>
2012-10-19 17:03:21 +0200

    Set the active sidebar link status with jQuery instead of with a Django filter

--- kitchen/dashboard/static/js/sidebar.js
+++ kitchen/dashboard/static/js/sidebar.js
@@ -1,79 +1,121 @@
-$(document).ready(function() {
-    setupClickHandlers();
-});
-
 function setupClickHandlers() {
     /**
-     * Assign click handlers to all sidebar entries.
+     * Assigns click handlers to all sidebar entries.
      */
-    $('.sidebar_link').click(function() {
-        var parameters = {};
-        var active_buttons = $('.active .sidebar_link');
-        if (active_buttons.index(this) > -1) {
-            active_buttons = active_buttons.not(this);
-        } else {
-            // active_buttons.add(this);
-            active_buttons = $.fn.add.call(active_buttons, this);
-        }
-        var changed_environment = false;
-        var changed_virt = false;
-        // Add already selected buttons to the params
-        for (var i=0; i < active_buttons.length; i++) {
-            var datatype = active_buttons[i].dataset['type'];
-            var dataname = active_buttons[i].dataset['name'];
-            if (datatype === 'env') {
-                if (active_buttons[i] === this) {
-                    parameters[datatype] = dataname;
-                    changed_environment = true;
-                } else if (!changed_environment) {
-                    parameters[datatype] = dataname;
-                }
-            } else if (datatype === 'virt') {
-                if (active_buttons[i] === this) {
-                    parameters[datatype] = dataname;
-                    changed_virt = true;
-                } else if (!changed_virt) {
-                    parameters[datatype] = dataname;
-                }
-            } else {
-                if (datatype in parameters && datatype !== 'env') {
-                    parameters[datatype] += ',' + dataname;
-                } else {
-                    parameters[datatype] = dataname;
-                }
-            }
-        }
 
-        // If there are no env or virt selections set parameter to an empty string
-        // to prevent server-side defaults from setting them
-        var datatypes = ['env', 'roles', 'options'];
-        if ( show_virt ) { datatypes.push('virt'); }
-        for (var datatype in datatypes) {
-            if (!(datatypes[datatype] in parameters)) {
-                parameters[datatypes[datatype]] = "";
-            }
-        }
+    $('li a.sidebar_link#env_link').click(function() {
+        var dataName = $(this).parent().attr('data-name');
 
-        // Build url parameters
-        var pathName = window.location.pathname;
-        if (pathName == '/'){
-            var url = 'api/nodes/?';
-            // Remove trailing '&'
-            for (var param in parameters) {
-                url += param + '=' + parameters[param] + '&';
-            }
-            // Draw new table
-            $.getJSON(url.slice(0, -1), function(data){
-                drawNodesTable(data);
-            });
+        if ($(this).parent().hasClass('active')) {
+            // Deactivate button
+            $(this).parent().removeClass('active');
         } else {
-            var url = '?';
-            // Remove trailing '&'
-            for (var param in parameters) {
-                url += param + '=' + parameters[param] + '&';
-            }
-            // Redirect to new url
-            window.location = url.slice(0, -1);
+            // Activate button and deactivate the rest
+            $(this).closest('ul').children('li').children('a.sidebar_link#env_link').parent().removeClass('active');
+            $(this).parent().addClass('active');
+        }
+        buildUrl();
+    });
+
+    $('li a.sidebar_link#role_link').click(function() {
+        var dataName = $(this).parent().attr('data-name');
+
+        if ($(this).parent().hasClass('active')) {
+            // Deactivate button
+            $(this).parent().removeClass('active');
+        } else {
+            // Activate button
+            $(this).parent().addClass('active');
+        }
+        buildUrl();
+    });
+
+    $('li a.sidebar_link#virt_link').click(function() {
+        var dataName = $(this).parent().attr('data-name');
+
+        if ($(this).parent().hasClass('active')) {
+            // Deactivate button
+            $(this).parent().removeClass('active');
+        } else {
+            // Activate button and deactivate the rest
+            $(this).closest('ul').children('li').children('a.sidebar_link#virt_link').parent().removeClass('active');
+            $(this).parent().addClass('active');
+        }
+        buildUrl();
+    });
+
+    $('li a.sidebar_link#options_link').click(function() {
+        var dataName = $(this).parent().attr('data-name');
+
+        if ($(this).parent().hasClass('active')) {
+            // Deactivate button
+            $(this).parent().removeClass('active');
+        } else {
+            // Activate button and deactivate the rest
+            $(this).closest('ul').children('li').children('a.sidebar_link#options_link').parent().removeClass('active');
+            $(this).parent().addClass('active');
+        }
+        buildUrl();
+    });
+}
+
+
+function buildUrl(){
+    var parameters = {};
+
+    // Collect env parameter
+    var env = $('li.active a.sidebar_link#env_link').parent().attr('data-name');
+    if (env == undefined) {
+        parameters['env'] = '';
+    } else {
+        parameters['env'] = env;
+    }
+
+    // Collect list of role parameters
+    $('li.active a.sidebar_link#role_link').each(function(index){
+        if (index == 0){
+            parameters['roles'] = $('li.active a.sidebar_link#role_link').parent().attr('data-name');
+        } else {
+            parameters['roles'] += ',' + $(this).parent().attr('data-name');
         }
     });
+
+    // Collect virt parameter
+    var virt = $('li.active a.sidebar_link#virt_link').parent().attr('data-name');
+    if (virt == undefined) {
+        parameters['virt'] = '';
+    } else {
+        parameters['virt'] = virt;
+    }
+
+    // Collect options parameters (graph)
+    $('li.active a.sidebar_link#options_link').each(function(index){
+        if (index == 0){
+            parameters['options'] = $('li.active a.sidebar_link#options_link').parent().attr('data-name');
+        } else {
+            parameters['options'] += ',' + $(this).parent().attr('data-name');
+        }
+    });
+
+    // Build url parameters
+    var pathName = window.location.pathname;
+    if (pathName == '/'){
+        var url = 'api/nodes/?';
+        // Set the url
+        for (var param in parameters) {
+            url += param + '=' + parameters[param] + '&';
+        }
+        // Draw new table
+        $.getJSON(url.slice(0, -1), function(data){
+            drawNodesTable(data);
+        });
+    } else {
+        var url = '?';
+        // Remove trailing '&'
+        for (var param in parameters) {
+            url += param + '=' + parameters[param] + '&';
+        }
+        // Redirect to new url
+        window.location = url.slice(0, -1);
+    }
 }
